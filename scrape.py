2689a6c2eee105663b04492022031d22754220d8
vartanbeno <vartanbeno@gmail.com>
2018-09-22 12:58:47 -0400

    finding tags

--- scrape.py
+++ scrape.py
@@ -1,7 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 with open('sample.html') as html_file:
     soup = BeautifulSoup(html_file, 'lxml')
 
-print(soup.prettify())
+
+'''
+Parse the whole file.
+'''
+# print(soup.prettify())
+
+
+'''
+Parse title tag.
+'''
+match = soup.title
+print(match)
+print(match.text)
+
+
+'''
+Search for div with class "footer".
+'''
+match = soup.find('div', class_='footer')
+print(match)
